c0d79387dfedae8244701752f9a86fcb82fb807a
olastrz <strzabalaaleksandra15@gmail.com>
2024-12-01 20:00:00 +0100

    Update to first iteration of tag selection

--- backend/tag_selection.py
+++ backend/tag_selection.py
@@ -1,13 +1,8 @@
 import pandas as pd
 import numpy as np
 
-def update_tags_rating(tags, update):
-    for tag in tags:
-        df = pd.read_csv(f"data/ratings.csv")
-        df_tags = np.array(df["tags"]) #change header in csv TODO
-        for tags_str in df_tags:
-            tags_arr = tags_str.split("|")
-            if tag in tags_arr:
-                if df.loc[df["tags"] == tags_str, "rating"] - update <= 10 and df.loc[df["tags"] == tags_str, "rating"] - update >=1:
-                    df.loc[df["tags"] == tags_str, "rating"] += update
-        df.to_csv(f"data/ratings.csv", index=False)
+def update_tags_rating(tags, update, cat_id):
+    df = pd.read_csv(f"data/ratings.csv")
+    newrow = pd.DataFrame({"userId": 1, "categoryId": cat_id, "rating": update, "tags": tags}, index=[0])
+    df = pd.concat([df, newrow], ignore_index=True)
+    df.to_csv(f"data/ratings.csv", index=False)
--- frontend/mock_backend_connection.py
+++ frontend/mock_backend_connection.py
@@ -8,14 +8,16 @@ def get_image_file_names():
 
 def like_callback(file_name):
     data = pd.read_csv("data/temp_images.csv")
-    tags = np.array(data[data["file_name"] == file_name.split("/")[-1].split(".")[0]]["tags"])[0].split("|")
-    ts.update_tags_rating(tags, 0.25)
+    filestem = file_name.split("/")[-1].split(".")[0]
+    tags = np.array(data[data["file_name"] == filestem]["tags"])[0]
+    cat_id = np.array(data[data["file_name"] == filestem]["categoryId"])[0]
+    ts.update_tags_rating(tags, 10, cat_id)
 
     print(file_name)
 
 
 def dislike_callback(file_name):
     data = pd.read_csv("data/temp_images.csv")
-    tags = np.array(data[data["file_name"] == file_name.split("/")[-1].split(".")[0]]["tags"])[0].split("|")
-    ts.update_tags_rating(tags, -0.25)
+    tags = np.array(data[data["file_name"] == file_name.split("/")[-1].split(".")[0]]["tags"])[0]
+    ts.update_tags_rating(tags, 1)
     print(file_name)

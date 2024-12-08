03f23224e6fc825e0d73ce5a6291875e219a4962
radek203 <r.porebinski@gmail.com>
2024-12-08 23:41:13 +0100

    code cleanup

--- backend/callbacks.py
+++ backend/callbacks.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 
+
 def rate_callback(user_id, ratings, category_id, tags, place_id):
     new_row = {
         "userId": user_id,
@@ -12,14 +13,15 @@ def rate_callback(user_id, ratings, category_id, tags, place_id):
     save_row_to_file(new_row)
     regenerate_images()
 
+
 def regenerate_images():
     if False not in st.session_state['is_image_rated'].values():
         for i in range(9):
             st.session_state['is_image_rated'][i] = False
             st.session_state['is_image_generate'][i] = False
         st.session_state['category_id'] = 1
-        st.session_state['last_id'] =+ 1
         print(st.session_state['is_image_rated'], st.session_state['is_image_generate'])
+
 
 def save_row_to_file(new_row):
     data = pd.read_csv("data/ratings.csv")
--- frontend/app_layout.py
+++ frontend/app_layout.py
@@ -13,7 +13,7 @@ def display_components(rows, method):
 
 class App:
     st.session_state['is_image_generate'] = {0: False, 1: False, 2: False, 3: False, 4: False, 5: False, 6: False, 7: False, 8: False}
-    st.session_state['is_image_rated']={0:False, 1:False, 2:False, 3:False, 4:False, 5:False, 6:False, 7:False, 8:False}
+    st.session_state['is_image_rated'] = {0: False, 1: False, 2: False, 3: False, 4: False, 5: False, 6: False, 7: False, 8: False}
     st.session_state['image_data'] = {0: [None, 0, 0, ''], 1: [None, 0, 0, ''], 2: [None, 0, 0, '']}
     st.session_state['last_id'] = 0
     st.session_state['category_id'] = 1
@@ -39,13 +39,12 @@ class App:
         img_data = st.session_state['image_data'][place_id]
         tile.image(img_data[0])
 
-    def create_rating_component(self,tile,place_id):
+    def create_rating_component(self, tile, place_id):
         if not st.session_state['is_image_rated'][place_id]:
             img_data = st.session_state['image_data'][place_id]
             rating = tile.slider("Rating", 1, 10, key=self.KEY_ID)
             self.KEY_ID += 1
-            tile.button("Rate", key=self.KEY_ID, use_container_width=True, on_click=mbc.rate_callback,
-                        args=(img_data[1], rating, img_data[2], img_data[3],place_id))
+            tile.button("Rate", key=self.KEY_ID, use_container_width=True, on_click=mbc.rate_callback, args=(img_data[1], rating, img_data[2], img_data[3], place_id))
             self.KEY_ID += 1
 
     def create_layout(self):
@@ -58,7 +57,5 @@ class App:
             row1 = layout.columns(3)
             row2 = layout.columns(3)
             row3 = layout.columns(3)
-            display_components(row1+row2+row3,self.create_image_container)
-            display_components(row1+row2+row3,self.create_rating_component)
-
-
+            display_components(row1 + row2 + row3, self.create_image_container)
+            display_components(row1 + row2 + row3, self.create_rating_component)
